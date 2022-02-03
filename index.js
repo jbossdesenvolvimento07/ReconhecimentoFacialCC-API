7f378a2112a3d61c522362620a36a1c47961523e
JBOSS - Vitor Bersani <desenvolvimento07@jboss.srv.br>
2022-02-03 17:32:17 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-/*import express from 'express';
-const app = express();
 
-//import '@tensorflow/tfjs-node';
-import * as canvas from 'canvas';
-import * as faceapi from 'face-api.js';
-const { Canvas, Image, ImageData, loadImage } = canvas
-faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
-
-import fs from 'fs';*/
-//const tensorflow = require('@tensorflow/tfjs');
+//const tf = require('@tensorflow/tfjs-node');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const fs = require('fs')
 const canvas = require('canvas')
 const faceapi = require('face-api.js');
-const { Canvas, Image, ImageData, loadImage } = canvas
+
+//
+//Configurações do FaceApi
+const { Canvas, Image, ImageData } = canvas
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
@@ -25,28 +20,141 @@ app.use(express.urlencoded({ limit: '50mb' }));
 
 
 var faceMatcher;
-var labeledFaceDescriptors1
-
-
-app.get('/', (req, res) => {
-    res.set({
-        //"Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Origin": "*"
-    });
-    res.send(labeledFaceDescriptors1)
-})
+var labeledFaceDescriptors = [faceapi.LabeledFaceDescriptors];
 
 
 //
-//----------------------------------------------
+//Endpoints
+
+//Retorna os usuários cadastrados
+app.get('/', (req, res) => {
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*"
+    });
+    res.send(labeledFaceDescriptors)
+})
+
+app.get('/salvar', (req, res) => {
+
+    saveLabeledFaces()
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*"
+    });
+    res.send(labeledFaceDescriptors)
+})
+
+//Adiciona Uma pessoa
 app.post('/adicionar', (req, res) => {
+
+    console.log('\nInício da requisição')
+    console.log('------------------------------------')
 
     const label = req.body.label
     const dataUrl = req.body.dataUrl
 
     adicionarPessoa(label, dataUrl, res)
 })
+
+//Valida uma foto
+app.post('/validar', (req, res) => {
+    
+    console.log('\nInício da requisição')
+    console.log('------------------------------------')
+
+
+    const dataUrl = req.body.dataUrl;
+    detectFace(dataUrl, res);
+})
+
+
+//Configura o listener e carrega redes neurais
+const port = 3030;
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+
+
+    console.log('Carregando Redes Neurais...')
+    Promise.all([
+        faceapi.nets.ssdMobilenetv1.loadFromDisk('./models'),
+        faceapi.nets.faceLandmark68Net.loadFromDisk('./models'),
+        faceapi.nets.ssdMobilenetv1.loadFromDisk('./models'),
+        faceapi.nets.faceRecognitionNet.loadFromDisk('./models'),
+    ]).then(start)
+})
+
+async function start() {
+    console.log('Carregando Faces...')
+
+    loadLabeledFaces();
+    
+    //labeledFaceDescriptors = await loadLabeledFacesFromLocalDir();
+
+    console.log('> Carregamento concluído <')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+async function saveLabeledFaces(){
+    const data = JSON.stringify(labeledFaceDescriptors);
+    fs.writeFile('C:/Projetos/ReconhecimentoFacialCC-API/labeledFaces.json', data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("> LabeledFaces salvas em JSON <");
+    });
+}
+
+function loadLabeledFaces(){
+    fs.readFile('C:/Projetos/ReconhecimentoFacialCC-API/labeledFaces.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+    
+        // parse JSON object
+
+        //labeledFaceDescriptors = JSON.parse(data.toString());
+        //labeledFaceDescriptors = data
+
+
+        const teste = JSON.parse(data.toString());
+
+
+        for (let i = 0; i < teste.length; i++) {
+            const descriptions = teste[i].descriptors;
+            const newPerson = new faceapi.LabeledFaceDescriptors(teste[i].label, descriptions)
+            labeledFaceDescriptors.push(newPerson)
+
+            console.log('CADASTRADO')
+        }
+
+
+    
+    });
+}
+
+
+
+
+
+
+
+
+
+
 async function adicionarPessoa(label, dataUrl, res){
     let image = new Image()
     image.src = dataUrl;
@@ -58,9 +166,7 @@ async function adicionarPessoa(label, dataUrl, res){
     }
 
     const newPerson = new faceapi.LabeledFaceDescriptors(label, descriptions)
-    labeledFaceDescriptors1.push(newPerson)
-
-
+    labeledFaceDescriptors.push(newPerson)
 
     res.set({
         "Content-Type": "application/json",
@@ -69,62 +175,8 @@ async function adicionarPessoa(label, dataUrl, res){
     });
     res.send({"Status": "Cadastrado"})
 
-    
-    // convert JSON object to string
-    const data = JSON.stringify(labeledFaceDescriptors1);
 
-    // write JSON string to a file
-    fs.writeFile('C:/teste.json', data, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("JSON data is saved.");
-    });
-
-}
-//----------------------------------------------
-//
-
-
-
-app.post('/api/verify', (req, res) => {
-
-    console.log('------------------------------------')
-    console.log('Requisição recebida...')
-
-    const dataUrl = req.body.dataUrl;
-
-    detectFace(dataUrl, res)
-
-})
-
-
-const port = 3030;
-
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-
-    Promise.all([
-        faceapi.nets.ssdMobilenetv1.loadFromDisk('./models'),
-        faceapi.nets.faceLandmark68Net.loadFromDisk('./models'),
-        faceapi.nets.ssdMobilenetv1.loadFromDisk('./models'),
-        faceapi.nets.faceRecognitionNet.loadFromDisk('./models'),
-    ]).then(start)
-})
-
-
-
-async function start() {
-    console.log('Carregando....')
-    //const labeledFaceDescriptors = await loadLabeledImages()
-
-    loadFromJSON();
-    //labeledFaceDescriptors1 = labeledFaceDescriptors
-
-    console.log('Carregamento concluido....')
-
-    //faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-   
+    saveLabeledFaces();
 }
 
 async function detectFace( dataUrl, res ){
@@ -132,14 +184,14 @@ async function detectFace( dataUrl, res ){
     let image = new Image()
     image.src = dataUrl;
 
-    faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors1, 0.6)
+    faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
     
-    console.log('Imagem convertida... ' + typeof(image))
+    console.log('Imagem convertida... ')
     
     const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
     console.log('Face detectada...')
     const results = await detections.map( (d) => faceMatcher.findBestMatch(d.descriptor))
-    console.log('Reconhecimento feito...')
+    console.log('> Reconhecimento <')
     console.log(results)
 
     res.set({
@@ -150,7 +202,29 @@ async function detectFace( dataUrl, res ){
     res.send(results)
 }
 
-function loadLabeledImages() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadLabeledFacesFromLocalDir() {
     const labels = ['Vitor Bersani', 'Andre']
     return Promise.all(
         labels.map(async label => {
@@ -166,17 +240,5 @@ function loadLabeledImages() {
             return new faceapi.LabeledFaceDescriptors(label, descriptions)
         })
     )
-}
-
-function loadFromJSON(){
-    fs.readFile('C:/teste.json', 'utf-8', (err, data) => {
-        if (err) {
-            throw err;
-        }
-    
-        // parse JSON object
-        labeledFaceDescriptors1 = JSON.parse(data.toString());
-    
-    });
 }
 
